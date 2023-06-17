2497ae7da1adb0e90e385c9ccfb196009c482d47
bhar-subhajit <subhoanindian@gmail.com>
2023-06-18 00:41:18 +0530

    booking api

--- routes/blog.js
+++ routes/blog.js
@@ -2,10 +2,19 @@ const express = require("express");
 const path = require("path");
 const blogs = require("../data/blogs");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const bodyParser = require("body-parser");
+const router = express.Router();
 
 const uri =
   "mongodb+srv://Subhajit:31223122@cluster0.6b1hmzh.mongodb.net/?retryWrites=true&w=majority";
-const router = express.Router();
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 router.get("/", (req, res) => {
   // res.sendFile(path.join(__dirname, '../templates/index.html'))
@@ -17,41 +26,11 @@ router.get("/movies", (req, res) => {
   // res.render('blogHome', {
   //     blogs: blogs
   // });
-  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
-  async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
+  getMovieList(req, res).catch(console.dir);
+});
 
-      const database = client.db("db");
-      const movies = database.collection("movies");
-      const query = { rating: { $gt: 7 } };
-      const options = {
-        // sort returned documents in ascending order by title (A->Z)
-        sort: { title: 1 },
-        // Include only the `title` and `imdb` fields in each returned document
-        // projection: { _id: 0, title: 1, imdb: 1 },
-      };
-      const cursor = movies.find(query, options);
-      const moviesArr = [];
-      for await (const doc of cursor) {
-        moviesArr.push(doc);
-      }
-      res.status(200);
-      res.json({ movies: moviesArr });
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-  }
-  run().catch(console.dir);
+router.post("/booking", bodyParser.json(), (req, res) => {
+  saveBooking(req, res).catch(console.dir);
 });
 
 router.get("/blogpost/:slug", (req, res) => {
@@ -65,5 +44,43 @@ router.get("/blogpost/:slug", (req, res) => {
   });
   // res.sendFile(path.join(__dirname, '../templates/blogPage.html'))
 });
+
+async function getMovieList(req, res) {
+  try {
+    const database = client.db("db");
+    const movies = database.collection("movies");
+    const query = { rating: { $gt: 4 } };
+    const options = {
+      // sort returned documents in ascending order by title (A->Z)
+      sort: { title: 1 },
+      // Include only the `title` and `imdb` fields in each returned document
+      // projection: { _id: 0, title: 1, imdb: 1 },
+    };
+    const cursor = movies.find(query, options);
+    const moviesArr = [];
+    for await (const doc of cursor) {
+      moviesArr.push(doc);
+    }
+    res.status(200);
+    res.json({ movies: moviesArr });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+async function saveBooking(req, res) {
+  try {
+    const database = client.db("db");
+    const bookings = database.collection("bookings");
+    const cursor = await bookings.insertOne(req.body);
+    console.log(cursor);
+    res.status(200);
+    res.json({ statusCode: "0", statusMsg: "Booking Saved Successfully" });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
 
 module.exports = router;
